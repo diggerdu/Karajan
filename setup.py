d7f0e21cdf0ef5bd17a1baa31a041b61653fd09c
diggerdu <diggerdu@hotmail.com>
2016-12-28 17:22:28 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
+from setuptools import setup
+
+setup(name='karajan',
+      version='0.1',
+      description='python audio analysis and synthesis toolbox',
+      url='http://github.com/digggerdu/karajan',
+      author='Du Xingjian',
+      author_email='diggerdu@hotmail.com',
+      license='GPL',
+      packages=['karajan'],
+      zip_safe=False)
 
 
