54619edc21212f42132e5f01741306706f51ff4c
diggerdu <diggerdu@hotmail.com>
2017-01-04 11:11:05 +0000

    slice

--- karajan/fft.py
+++ karajan/fft.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import numpy as np
-import scipy 
+import scipy
+
 
 def stft(wav, n_fft=1024, overlap=4, dt=tf.int16, absp=False):
     assert (wav.shape[0] > n_fft)
     X = tf.plcaeholder(dtype=dt,shape=wav.shape)
     S = tf.fft(X)
     hop = n_fft / overlap
-    W = tf.constant(scipy.hanning(n_fft), dtype=tf.float32)
+    
+    ## prepare constant variable
     Pi = tf.constant(np.pi, dtype=tf.float32)
-
-
+    W = tf.constant(scipy.hanning(n_fft), dtype=tf.float32)
+    
--- karajan/test.py
+++ karajan/test.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 
 
-a = tf.placeholder(tf.int16, shape=[1])
-x = a
-for i in range(6):
-    x = tf.add(x,x)
+a = tf.placeholder(tf.int16, shape=[10])
+x = tf.strided_slice(a, [0,], [9,])
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
